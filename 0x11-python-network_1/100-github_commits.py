74f98c0bcc7160f6f30d752e420c44e5322698d8
Fortunatus <wulnyefortunatus@gmail.com>
2023-04-29 04:28:43 +0000

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
+"""A python script that lists the 10 most recent commits on a given GitHub repository.
 
 Usage: ./100-github_commits.py <repository name> <repository owner>
 """

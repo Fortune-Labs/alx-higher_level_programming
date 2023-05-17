eaa0aa83aa6a60efadcd8c1d06513dcb32f4e0db
Fortunatus <wulnyefortunatus@gmail.com>
2023-05-17 01:30:52 +0000

    changes made

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,10 @@
 """A script that fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
+url = 'https://alx-intranet.hbtn.io/status'
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
+    with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))

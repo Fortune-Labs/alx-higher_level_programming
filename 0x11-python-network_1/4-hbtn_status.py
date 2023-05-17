810437a8e980dac3977e9d7d3dd1cb0a04c4e047
Fortunatus <wulnyefortunatus@gmail.com>
2023-05-17 01:56:02 +0000

    changes made

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """A python script that fetches https://intranet.hbtn.io/status."""
-import urllib.request
+import requests
 
-url = 'https://alx-intranet.hbtn.io/status'
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-        print("Body response:")
-        print("\t- type: {}".format(type(r.text)))
-        print("\t- content: {}".format(r.text))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
